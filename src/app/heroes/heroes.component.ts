f07f0f42ebd5f72591e7d849b5a9ee2bf862fe37
andreshiguitaperez <32747704+andreshiguitaperez@users.noreply.github.com>
2022-06-20 23:25:22 -0500

    Realizacion del punto 2
    Se realizo la funcion de mostrar listas

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
 
   hero: Hero = {
     id: 1,
@@ -17,5 +21,9 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
